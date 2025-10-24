a459bfb0e1fd3abeb0e6f2552005f815a84e109d
Ceciliaiii <tndnpr@163.com>
2025-10-24 23:04:24 +0800

    feat: 实现对话列表排序和搜索

--- renderer/components/ConversationList/useFilter.ts
+++ renderer/components/ConversationList/useFilter.ts
@@ -1,15 +1,91 @@
 import { useConversationsStore } from '../../stores/conversations';
+import { debounce } from '../../../common/utils';
+import type { Conversation } from '../../../common/types';
 
-const searchKey = ref('');
 
 // 对话筛选器
+const searchKey = ref('');
+const _searchKey = ref('');
 export function useFilter() {
   const conversationsStore = useConversationsStore();
 
-  const filteredConversations = computed(() => {
+  // 排序部分
+  const sortConversations = computed(() => {
+    const { sortBy, sortOrder } = conversationsStore.sortMode;
 
-    return conversationsStore.allConversations
-  })
+    const divider = Object.freeze({
+      type: 'divider',
+      id: -1
+    }) as Conversation;
+
+    // 置顶过的对话项数组
+    const pinned: Conversation[] = conversationsStore.allConversations.filter(item => item.pinned).map(item => ({ type: 'conversation', ...item }));
+
+    // 对话项置顶的话，将分割线推到该项底部
+    if (pinned.length) {
+      pinned.push(divider);
+    }
+
+    // 未置顶过的对话项数组
+    const unPinned: Conversation[] = conversationsStore.allConversations.filter(item => !item.pinned).map(item => ({ type: 'conversation', ...item }));
+
+
+    // 排序方法
+    const handleSortOrder = <T = number | string>(a?: T, b?: T) => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        return sortOrder === 'desc' ? b - a : a - b;
+      }
+
+      if (typeof a === 'string' && typeof b === 'string') {
+        return sortOrder === 'desc' ? b.localeCompare(a) : a.localeCompare(b);
+      }
+
+      return 0
+    }
+
+    if (sortBy === 'createAt') {
+      return [
+        ...pinned.sort((a, b) => handleSortOrder(a.createdAt, b.createdAt)),
+        ...unPinned.sort((a, b) => handleSortOrder(a.createdAt, b.createdAt)),
+      ]
+    }
+
+    if (sortBy === 'updatedAt') {
+      return [
+        ...pinned.sort((a, b) => handleSortOrder(a.updatedAt, b.updatedAt)),
+        ...unPinned.sort((a, b) => handleSortOrder(a.updatedAt, b.updatedAt)),
+      ]
+    }
+
+    if (sortBy === 'name') {
+      return [
+        ...pinned.sort((a, b) => handleSortOrder(a.title, b.title)),
+        ...unPinned.sort((a, b) => handleSortOrder(a.title, b.title)),
+      ]
+    }
+
+
+    return [
+      ...pinned.sort((a, b) => handleSortOrder(a.selectedModel, b.selectedModel)),
+      ...unPinned.sort((a, b) => handleSortOrder(a.selectedModel, b.selectedModel)),
+    ]
+  });
+
+
+  // 搜索部分
+  const filteredConversations = computed(() => {
+    if (!_searchKey.value) return sortConversations.value;
+    // 返回标题包含搜索关键字的对话项
+    return sortConversations.value.filter(item => item?.title && item?.title.includes(_searchKey.value));
+  });
+
+
+  // 更新搜索关键字
+  const updateSearchKey = debounce((val) => {
+    _searchKey.value = val;
+  }, 200);
+
+  watch(() => searchKey.value, (val) => updateSearchKey(val));
 
   return {
     searchKey,
